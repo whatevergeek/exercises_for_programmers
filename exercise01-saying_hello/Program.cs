a6d45929dcdf77ab31a24590a3ada3146caf4cd7
Paul Amazona <netfiddler@yahoo.com>
2017-11-12 08:52:48 +0800

    version01 Keep the input, stringconcatenation, and output separate.

--- exercise01-saying_hello/Program.cs
+++ exercise01-saying_hello/Program.cs
@@ -6,7 +6,10 @@ namespace exercise01_saying_hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}, nice to meet you! ");
+            Console.ReadLine();
         }
     }
 }
