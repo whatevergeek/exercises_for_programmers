681a7a414a95c9a2515e33e4e07f4282ed73b5ee
Paul Amazona <netfiddler@yahoo.com>
2017-11-12 08:44:54 +0800

    Add project files.

---
+++ exercise01-saying_hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise01_saying_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
