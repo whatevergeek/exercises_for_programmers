1237fbe8da17af316359373aacc021036d2fe86c
Paul Amazona <netfiddler@yahoo.com>
2017-11-12 09:20:54 +0800

    exercise01-version01
    Constraints
    • Besuretheoutputcontainstheoriginalstring.
    • Useasingleoutputstatementtoconstructtheoutput.
    • Useabuilt-infunctionoftheprogramminglanguageto determinethelengthofthestring.

---
+++ exercise02-counting_the_number_of_characters/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace exercise02_counting_the_number_of_characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("What is the input string? ");
+            var input = ReadLine();
+            WriteLine($"{input} has {input.Length} characters.");
+            ReadLine();
+        }
+    }
+}
