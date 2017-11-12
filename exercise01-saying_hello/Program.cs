43d0179a1b4181fa5d334d0a8d41bb45c8b0a4c7
Paul Amazona <netfiddler@yahoo.com>
2017-11-12 09:07:06 +0800

    version02 Write a new version of the program without using any variables.

--- exercise01-saying_hello/Program.cs
+++ exercise01-saying_hello/Program.cs
@@ -7,8 +7,7 @@ namespace exercise01_saying_hello
         static void Main(string[] args)
         {
             Console.Write("What is your name? ");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Hello, {name}, nice to meet you! ");
+            Console.WriteLine($"Hello, {Console.ReadLine()}, nice to meet you! ");
             Console.ReadLine();
         }
     }
