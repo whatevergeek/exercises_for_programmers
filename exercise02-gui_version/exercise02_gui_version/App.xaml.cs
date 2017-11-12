af5b268cd46d8a19854e6c9f8a072edbc783a8c9
Paul Amazona <netfiddler@yahoo.com>
2017-11-12 11:43:49 +0800

    exercise02-version03
    Challenge:
    Implementthisprogramusingagraphicaluserinterface and update the character counter every time a key is pressed.

---
+++ exercise02-gui_version/exercise02_gui_version/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace exercise02_gui_version
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new exercise02_gui_version.MainPage();
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ exercise02-gui_version/exercise02_gui_version/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace exercise02_gui_version
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            EntryBox.TextChanged += EntryBox_TextChanged;
+        }
+
+        private void EntryBox_TextChanged(object sender, EventArgs e)
+        {
+            CountLabel.Text = EntryBox.Text.Length > 0 ?
+                $"{EntryBox.Text} has {EntryBox.Text.Length} characters. " :
+                "";
+        }
+    }
+}
